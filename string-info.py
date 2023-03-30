aa4ae76bec74af97146519e15834312054b24bc6
lucafus <luca.fusaro1996@gmail.com>
2023-03-30 17:56:02 +0000

    new Python practice

---
+++ string-info.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3.7
+
+message = input('Enter a lovely message: ')
+
+print('First character:', message[0])
+print('Last Character:', message[-1])
+print( 'Middle Character:', message[int(len(message) / 2)])
+print( 'Reverse message:', message[::-1])
+
+
+
+
