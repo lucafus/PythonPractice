c2d12c49b9513e868ac8d3a392848566088e2b23
lucafus <luca.fusaro1996@gmail.com>
2023-04-07 19:40:47 +0000

    Barista Robot 2.0

--- Practice0407.py
+++ Practice0407.py
@@ -4,16 +4,26 @@ name = input("What is your name? ")
 menu = "Latte Machiato, Coffee, Black Coffee "
 price =  8 
 
-print("Hello " + name + " what do you want from our menu today? Here is what we have " + menu)
+
+if name == "Pepe":
+    print("Your are not welcome here Paloma!")
+    exit()
+else:
+    print("Hello " + name + " what do you want from our menu today? Here is what we have " + menu)
+    
 
 order = input ()
 
+
 print("How many " + order + " " + "do you want?")
+
 
 ammount = input()
 
 total = price * int(ammount)
 
+
 print ("Thank you!, your total is:" + " " + str(total) + " USD")
+
 print ("We will have your " + ammount + " " + order + " in a moment")
 
