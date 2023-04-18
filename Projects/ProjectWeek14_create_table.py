9d49b079315c4e16b84e40a9139bb9cb542046f1
lucafus <luca.fusaro1996@gmail.com>
2023-04-18 20:51:01 +0000

    Project Week 14

--- Projects/ProjectWeek14_create_table.py
+++ Projects/ProjectWeek14_create_table.py
@@ -6,43 +6,35 @@ dynamodb = boto3.resource('dynamodb', region_name= 'us-east-2')
 
 #Call the create_table method
 
-table = dynamodb.create_table( 
+response = dynamodb.create_table( 
     
     TableName= 'Video_Games',
     
     AttributeDefinitions=[ 
             
-                {
+            {
             
             'AttributeName': 'title', # Specify the name of the title attribute
             'AttributeType' : 'S'     # Specify that the title attribute is a string
             
-            
-                    }
-    ],
-    
-    
+            }
+         ],
+   
     KeySchema=[ 
             
-                {   
+            {   
             
             'AttributeName': 'title', # Specify that the title attribute will be used as the partition key
             'KeyType' : 'HASH'        # Specify that the title attribute will be used as the hash key
                         
-                    }
-    ],
-    
-    
-    
-                    
+            }
+         ],
+                      
     ProvisionedThroughput={
         'ReadCapacityUnits': 1,
         'WriteCapacityUnits' : 1
-        
-        
-    }        
+          
+            }        
 )
 
-print("The Video_Game table has been created")
-
-
+print(response)
